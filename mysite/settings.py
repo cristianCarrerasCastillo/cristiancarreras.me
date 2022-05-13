209fbcde7e6b75d6ebce91572dc96f3711679725
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-05-13 00:35:40 -0400

    add key for database

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': 3306,
     }
 }
 
