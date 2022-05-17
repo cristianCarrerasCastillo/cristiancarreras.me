0a82dfa0265aefd87dc58789e4c37fcaf41fbaa7
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-05-16 23:18:51 -0400

    add logo field in db of post experience

--- PostExperiencia/models.py
+++ PostExperiencia/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+#ToDo:
+# - Create a repo for images
+# - Delete hrs in inicio and final when creating a new post, only have a day
 
 class Post(models.Model):
     empresa = models.CharField(max_length=200)
     body = models.TextField()
     inicio = models.DateTimeField()
     final = models.DateTimeField()
+    logo = models.ImageField()
     def __str__(self):
         return self.empresa
