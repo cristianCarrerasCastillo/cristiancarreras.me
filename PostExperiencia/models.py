3eb4d3ccb4c7196ec60d960d8df3730f85fe0ee7
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-05-17 21:56:34 -0400

    fix display date of when work started and end date

--- PostExperiencia/models.py
+++ PostExperiencia/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     empresa = models.CharField(max_length=200, verbose_name='Empresa')
     cargo = models.CharField(max_length=100, verbose_name='Cargo')
     body = models.TextField(verbose_name='Descripción del puesto')
-    inicio = models.DateTimeField(verbose_name='Fecha de inicio',blank=True, null=True)
-    final = models.DateTimeField(verbose_name='Fecha de finalización', blank=True, null=True)
+    inicio = models.DateField(verbose_name='Fecha de inicio',blank=True, null=True)
+    final = models.DateField(verbose_name='Fecha de finalización', blank=True, null=True)
     logo = models.ImageField(verbose_name='Logo de la empresa', upload_to='logos', blank=True, null=True)
 
     class Meta:
--- PostExperiencia/views.py
+++ PostExperiencia/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from PostExperiencia.models import Post
-from .models import Post
+from .models import Post, Bio, skills
 
 # Create your views here.
 def cv(request):
     post = Post.objects.all()
-    return render(request, 'index.html', {'post': post})
+    bio = Bio.objects.all()
+    #skills = skills.objects.all()
+    return render(request, 'index.html', {'post': post, 'bio': bio})
 
