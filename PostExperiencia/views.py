aca6ce54a40096a859bbf36e404f66ccbbda674c
Cristian Carreras <cristian.carrerascastillo@gmail.com>
2022-05-18 22:12:11 -0400

    change in the method of access to bio info

--- PostExperiencia/views.py
+++ PostExperiencia/views.py
@@ -5,7 +5,7 @@ from .models import Post, Bio, skills
 # Create your views here.
 def cv(request):
     post = Post.objects.all().order_by('-inicio')
-    bio = Bio.objects.all()
+    bio = Bio.objects.first()
     #skills = skills.objects.all()
     return render(request, 'index.html', {'post': post, 'bio': bio})
 
